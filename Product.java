97cfdb16361820d97fc59a646c9bbccd89e0464f
Delowar Husain <138960860+delowarshizar@users.noreply.github.com>
2023-08-02 00:25:05 +0600

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,8 @@
+public class Product {
+    public static void main(String[] args)
+    {
+        System.out.println("Id: 101,\nName: iphone15, \nPrice: 1895 euros");
+        System.out.println("Description: Perfect product with best images quality,\ncategory: phone");
+    }
+
+}
