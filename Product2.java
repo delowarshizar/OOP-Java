fc896869b37d6dec8a06ecd4ac45379907565015
Delowar Husain <138960860+delowarshizar@users.noreply.github.com>
2023-08-03 08:08:30 +0600

    Add files via upload

---
+++ Product2.java
@@ -0,0 +1,17 @@
+public class Product2 {
+    public static void main(String[] args)
+    {
+        int id = 101;
+        String title,price,description,category;
+        title = "iphone15";
+        price = "1895 euros";
+        description ="perfect product with best image quality";
+        category ="phone";
+
+        System.out.println("Id: "+id);
+        System.out.println("Title: "+title);
+        System.out.println("Price: "+price);
+        System.out.println("Description: "+description);
+        System.out.println("Category: "+category);
+    }
+}
