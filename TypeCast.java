cbad53a70f4937b58efe75fa778b21d4585e7098
Delowar Husain <138960860+delowarshizar@users.noreply.github.com>
2023-12-09 00:24:39 +0600

    TypeCast.java

---
+++ TypeCast.java
@@ -0,0 +1,16 @@
+
+ class TypeCast {
+     public static void main(String[] args) {
+
+         int a = 54;
+         byte c = (byte) a; //casting big to small
+         System.out.println(c);
+
+
+         byte d = 56;
+         int b = d; //small to big conversion.
+         System.out.println(b);
+
+
+     }
+ }
