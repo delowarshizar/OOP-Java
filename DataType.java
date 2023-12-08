919cf92f93c8d24d29481dacb607e03f2bfa87e0
Delowar Husain <138960860+delowarshizar@users.noreply.github.com>
2023-12-09 00:04:34 +0600

    DataType.java

---
+++ DataType.java
@@ -0,0 +1,19 @@
+class DataType {
+     public static void main(String[] args) {
+         float marks = 70.6f;
+         int a = 54;
+         long b = 64;
+         String x = "Delowar Husain";
+         double y = 4.4;
+         char c = 'k';
+         boolean t = true;
+
+         System.out.println(marks);
+         System.out.println(a);
+         System.out.println(b);
+         System.out.println(x);
+         System.out.println(y);
+         System.out.println(c);
+         System.out.println(t);
+     }
+ }
